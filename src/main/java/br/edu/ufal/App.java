718e4bc7b41c9930d01d741c61143f1efbf2cabb
andersonjso <anderson.jose.so@gmail.com>
2016-02-07 23:52:31 -0300

    [improv] Add jooby files

---
+++ src/main/java/br/edu/ufal/App.java
@@ -0,0 +1,18 @@
+package br.edu.ufal;
+
+import org.jooby.Jooby;
+
+/**
+ * @author jooby generator
+ */
+public class App extends Jooby {
+
+  {
+    get("/", () -> "Hello World!");
+  }
+
+  public static void main(final String[] args) throws Exception {
+    new App().start(args);
+  }
+
+}
